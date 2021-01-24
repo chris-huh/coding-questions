10e66495e364f6062735403a570f86943e7439a1
chris-huh <hsmysk@gmail.com>
2021-01-23 22:15:27 -0500

    Addon to 344

--- LeetCode/344-ReverseString.js
+++ LeetCode/344-ReverseString.js
@@ -15,9 +15,7 @@ var reverseString = function (s) {
 	let start = 0,
 		end = s.length - 1;
 	while (start < end) {
-		let temp = s[start];
-		s[start] = s[end];
-		s[end] = temp;
+		[s[start], s[end]] = [s[end], s[start]]; // new in ES6!
 		start++;
 		end--;
 	}
